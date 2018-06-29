78e8dbd8c456afc32c377d5d202958bb9a0789a2
Daniel Passos <daniel@passos.me>
2018-06-29 16:02:11 -0300

    Retrieve memes

--- app/src/main/java/org/aerogear/android/app/memeolist/ui/MainActivity.java
+++ app/src/main/java/org/aerogear/android/app/memeolist/ui/MainActivity.java
@@ -2,8 +2,21 @@ package org.aerogear.android.app.memeolist.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
 
 import org.aerogear.android.app.memeolist.R;
+import org.aerogear.android.app.memeolist.graphql.ListMemesQuery;
+import org.aerogear.mobile.core.MobileCore;
+import org.aerogear.mobile.sync.SyncService;
+
+import java.util.List;
+
+import javax.annotation.Nonnull;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,4 +26,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void getMemes(View view) {
+        ApolloClient apolloClient = SyncService.getInstance().getApolloClient();
+
+        apolloClient
+                .query(ListMemesQuery.builder().build())
+                .enqueue(new ApolloCall.Callback<ListMemesQuery.Data>() {
+                    @Override
+                    public void onResponse(@Nonnull Response<ListMemesQuery.Data> response) {
+                        List<ListMemesQuery.AllMeme> memes = response.data().allMemes();
+                        MobileCore.getLogger().debug("Total memes: " + memes.size());
+                    }
+
+                    @Override
+                    public void onFailure(@Nonnull ApolloException e) {
+                        MobileCore.getLogger().error(e.getMessage(), e);
+                    }
+                });
+    }
 }
