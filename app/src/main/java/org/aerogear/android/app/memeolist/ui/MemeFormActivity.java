bdb6d437040745845676da0d43758fa57894a803
Daniel Passos <daniel@passos.me>
2018-07-02 19:36:56 -0300

    Upload image to imgur

--- app/src/main/java/org/aerogear/android/app/memeolist/ui/MemeFormActivity.java
+++ app/src/main/java/org/aerogear/android/app/memeolist/ui/MemeFormActivity.java
@@ -1,20 +1,43 @@
 package org.aerogear.android.app.memeolist.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.bumptech.glide.Glide;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 import org.aerogear.android.app.memeolist.R;
+import org.aerogear.mobile.core.MobileCore;
+import org.aerogear.mobile.core.executor.AppExecutors;
+import org.aerogear.mobile.core.reactive.Requester;
+import org.aerogear.mobile.core.reactive.Responder;
+import org.json.JSONObject;
 
+import java.io.File;
 import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class MemeFormActivity extends AppCompatActivity {
+
+    @BindView(R.id.meme)
+    ImageView mMemeImage;
 
     @BindView(R.id.topText)
     TextView mTopText;
@@ -27,6 +50,8 @@ public class MemeFormActivity extends AppCompatActivity {
 
     @BindView(R.id.bottomTextPreview)
     TextView mBottomTextPreview;
+
+    private File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +73,7 @@ public class MemeFormActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-                mtopTextPreview.setText(editable);
+                mtopTextPreview.setText(editable.toString());
             }
         });
 
@@ -63,7 +88,7 @@ public class MemeFormActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-                mBottomTextPreview.setText(editable);
+                mBottomTextPreview.setText(editable.toString());
             }
         });
     }
@@ -75,6 +100,109 @@ public class MemeFormActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                file = new File(result.getUri().getPath());
+                Glide.with(mMemeImage).load(file).into(mMemeImage);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                MobileCore.getLogger().error(error.getMessage(), error);
+                displayMessage(R.string.error_gettting_image);
+            }
+        }
+    }
+
+    @OnClick(R.id.meme)
+    void choiceImage() {
+        CropImage.activity()
+                .setAspectRatio(1, 1)
+                .setAutoZoomEnabled(false)
+                .start(this);
+    }
+
+    @OnClick(R.id.send)
+    void send() {
+        if (isValid()) {
+
+            MaterialDialog materialDialog = new MaterialDialog.Builder(this)
+                    .title(R.string.creating_meme)
+                    .content(R.string.please_wait)
+                    .progress(true, 0)
+                    .cancelable(false)
+                    .show();
+
+            uploadImage()
+                    .respondOn(new AppExecutors().mainThread())
+                    .respondWith(new Responder<String>() {
+                        @Override
+                        public void onResult(String imageUrl) {
+                            materialDialog.dismiss();
+                            finish();
+                        }
+
+                        @Override
+                        public void onException(Exception exception) {
+                            materialDialog.dismiss();
+                            MobileCore.getLogger().error(exception.getMessage(), exception);
+                            displayMessage(R.string.error_upload);
+                        }
+                    });
+
+        }
+    }
+
+    private org.aerogear.mobile.core.reactive.Request<String> uploadImage() {
+
+        return Requester.call(() -> {
+
+            RequestBody requestBody = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("type", "file")
+                    .addFormDataPart("image", file.getName(),
+                            RequestBody.create(MediaType.parse("text/image/jpeg"), file))
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url("https://api.imgur.com/3/image")
+                    .addHeader("Authorization", "Client-ID f1041ec178352c6")
+                    .addHeader("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW")
+                    .post(requestBody)
+                    .build();
+
+            OkHttpClient client = new OkHttpClient.Builder().build();
+            okhttp3.Response response = client.newCall(request).execute();
+            JSONObject jsonResponse = new JSONObject(response.body().string());
+            return jsonResponse.getJSONObject("data").getString("link");
+
+        }).requestOn(new AppExecutors().networkThread());
+
+    }
+
+    private boolean isValid() {
+
+        if (file == null) {
+            new MaterialDialog.Builder(this)
+                    .title(R.string.meme_create_meme)
+                    .content(R.string.meme_need_image)
+                    .positiveText(R.string.ok)
+                    .cancelable(false)
+                    .show();
+            return false;
+        }
+
+        return true;
+
+    }
+
+    private void displayMessage(@StringRes int resId) {
+        Toast.makeText(getApplicationContext(), resId, Toast.LENGTH_SHORT).show();
     }
 
 }
