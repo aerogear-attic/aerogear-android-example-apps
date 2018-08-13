5876bdccb6af815f67097b6283d6477963e993a9
Daniel Passos <daniel@passos.me>
2018-08-13 17:59:51 -0300

    Fix message handler

--- memeolist/app/src/main/java/org/aerogear/android/app/memeolist/ui/BaseActivity.java
+++ memeolist/app/src/main/java/org/aerogear/android/app/memeolist/ui/BaseActivity.java
@@ -1,7 +1,6 @@
 package org.aerogear.android.app.memeolist.ui;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
@@ -13,8 +12,8 @@ public abstract class BaseActivity extends AppCompatActivity {
     private MessageHelper messageHelper;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         this.messageHelper = new MessageHelper(getApplicationContext());
     }
