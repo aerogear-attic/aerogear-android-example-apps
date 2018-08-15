10c57064f919e2b0c516ddb41ca7b13c054dee85
Daniel Passos <daniel@passos.me>
2018-08-15 15:51:38 -0300

    Disable login button on click

--- memeolist/app/src/main/java/org/aerogear/android/app/memeolist/ui/LoginActivity.java
+++ memeolist/app/src/main/java/org/aerogear/android/app/memeolist/ui/LoginActivity.java
@@ -3,6 +3,8 @@ package org.aerogear.android.app.memeolist.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
 
 import org.aerogear.android.app.memeolist.R;
 import org.aerogear.mobile.auth.AuthService;
@@ -11,13 +13,16 @@ import org.aerogear.mobile.auth.user.UserPrincipal;
 import org.aerogear.mobile.core.Callback;
 import org.aerogear.mobile.core.MobileCore;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private static final String TAG = LoginActivity.class.getName();
+    @BindView(R.id.login)
+    Button mLogin;
 
+    private static final String TAG = LoginActivity.class.getName();
     private static final int LOGIN_RESULT_CODE = 9831;
 
     private AuthService authService;
@@ -41,17 +46,24 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.login)
     void login() {
-        DefaultAuthenticateOptions options = new DefaultAuthenticateOptions(this, LOGIN_RESULT_CODE);
+        mLogin.setEnabled(false);
+
+        DefaultAuthenticateOptions options = new DefaultAuthenticateOptions(
+                this, LOGIN_RESULT_CODE);
 
         authService.login(options, new Callback<UserPrincipal>() {
             @Override
             public void onSuccess(UserPrincipal models) {
-                MobileCore.getLogger().info(TAG, models.getEmail());
+                startActivity(new Intent(getApplicationContext(), MemeListActivity.class));
             }
 
             @Override
             public void onError(Throwable error) {
                 MobileCore.getLogger().error(TAG, error.getMessage(), error);
+                mLogin.setEnabled(true);
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+
             }
         });
     }
