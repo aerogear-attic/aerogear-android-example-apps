08763b2b8c5d5dd33f91addedf921b91bba83319
Daniel Passos <daniel@passos.me>
2018-08-30 10:24:37 -0300

    Update fetch_graphql_schema.sh to use localhost

--- memeolist/fetch_graphql_schema.sh
+++ memeolist/fetch_graphql_schema.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p ./app/src/main/graphql/org/aerogear/android/app/memeolist/graphql
-apollo-codegen download-schema https://api.graph.cool/simple/v1/cjiyvc1wa40kg011846ev0ff8 --output ./app/src/main/graphql/org/aerogear/android/app/memeolist/graphql/schema.json
+apollo-codegen download-schema http://localhost:8000/graphql --output ./app/src/main/graphql/org/aerogear/android/app/memeolist/graphql/schema.json
 
